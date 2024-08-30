c91e204032cea6dc13c5cf2db52c9217d9336640
pedroryansc <pedroryangamer@gmail.com>
2024-08-30 01:57:39 -0300

    Aula 8 - Relacionamento N:N

---
+++ app/Models/Produto.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produto extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ["descricao"];
+
+    public function vendas(){
+        return $this->belongsToMany("App\Models\Pedido", "pedido__produtos")
+                    ->withPivot("valorUnitario", "quantidade");
+    }
+}
